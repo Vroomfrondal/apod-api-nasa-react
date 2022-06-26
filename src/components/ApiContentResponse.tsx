e71ee98e7073bcf84b90753a379254a73e7a9077
Vroomfrondal <lionscantcode@gmail.com>
2022-06-25 19:06:11 -0500

    Finish TS to display API response

--- src/components/ApiContentResponse.tsx
+++ src/components/ApiContentResponse.tsx
@@ -4,12 +4,10 @@ import dotenv from 'dotenv'
 import './ApiContentResponse.css'
 
 function ApiContentResponse() {
-  const [data, setData] = useState({})
-
   const fetchData = async () => {
     try {
       const apiKey = process.env.REACT_APP_NASA_API_KEY
-      const url: string = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
       const apiResponse = await axios.get(url)
 
       if (apiResponse.status === 200) {
@@ -17,51 +15,60 @@ function ApiContentResponse() {
         console.log(apiData)
 
         displayRequestedData(apiData)
-      } else alert('Nasa seems to be having an issue with their server.')
+      } else throw new Error('Nasa seems to be having an issue with their server.')
     } catch (err) {
       console.log('Nasa seems to be having an issue with their server.')
     }
   }
 
   const displayRequestedData = (data: any) => {
-    const date = document.querySelector('#date') as HTMLParagraphElement
-    const title = document.querySelector('#title') as HTMLHeadingElement
-    const copyright = document.querySelector('#copyright') as HTMLElement
-    const information = document.querySelector(
-      '#description'
-    ) as HTMLParagraphElement
-    const currentDate = new Date().toISOString().slice(0, 10) as string
-    const mediaSection = document.querySelector(
-      '#media-section'
-    ) as HTMLParagraphElement
-    const imageSection = `<a id="hdimg" href="" target="_blank" rel="noopener">
-                            <div class="image-div"> 
-                                <img id="image_of_the_day" src="" alt="image-by-nasa"> 
-                            </div>
-                          </a>`
-    const videoSection = `<div class="video-div"> 
-                            <iframe id="videoLink" src="" frameborder="0"></iframe>
-                          </div>`
+    const dateEl = document.querySelector('#date') as HTMLParagraphElement
+    const titleEl = document.querySelector('#title') as HTMLHeadingElement
+    const copyrightEl = document.querySelector('#copyright') as HTMLElement
+    const photoExplanationEl = document.querySelector('#description') as HTMLParagraphElement
+    const todaysDate = new Date().toISOString().slice(0, 10) as string
+    const mediaSectionEl = document.querySelector('#media-section') as HTMLParagraphElement
+    const imageSectionEl = `<a id="img-new-tab" href="" target="_blank" rel="noopener">
+                              <div class="image-div"> 
+                                  <img id="image_of_the_day" src="" alt="image-by-nasa"> 
+                              </div>
+                            </a>` as any
+    const videoSectionEl = `<div class="video-div"> 
+                              <iframe id="videoLink" src="" frameborder="0"></iframe>
+                            </div>` as any
 
-    // Display Title, Date, and Media Description
-    title.innerHTML = data.title
-    date.innerHTML = data.date
-    copyright.innerHTML = data.copyright
+    // Title, Date, Media Copyright, Photo Explanation
+    titleEl.innerHTML = data.title
+    dateEl.innerHTML = data.date
+    copyrightEl.innerHTML = data.copyright
+    photoExplanationEl.innerHTML = data.explanation
+
+    // Dynamically supply HTML element based on video or img API response
+    if (data.media_type === 'video') {
+      mediaSectionEl.innerHTML = videoSectionEl
+      videoSectionEl!.src = data.url
+    } else {
+      mediaSectionEl.innerHTML = imageSectionEl
+      document.querySelector<HTMLAnchorElement>('#img-new-tab')!.href =
+        data.hdurl
+      document.querySelector<HTMLImageElement>('#image_of_the_day')!.src =
+        data.url
+    }
   }
 
   fetchData()
 
   return (
     <>
-      <section>
-        <h2 id="title"></h2>
+      <section className="media-details">
+        <h2 id="title">Loading...</h2>
         <p id="date"></p>
         <p id="credits-wrapper">Media courtsey:</p>
-        <p id="copyright"></p>
+        <p id="copyright">NASA</p>
       </section>
 
-      <section id="media-section">
-        {/* <!-- Response from API goes here --> */}
+      <section className="media" id="media-section">
+        {/* <!-- Media Response from API goes here --> */}
       </section>
 
       <div className="description-div">
