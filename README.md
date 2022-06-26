9d1beac36ab30929e72a72c75f31f47404b8d444
Christopher DeLeon <lionscantcode@gmail.com>
2022-06-25 20:19:10 -0500

    API Data Display (#4)
    * header HTML & JS completion (#3)
    
    * Finish TS to display API response
    
    * CSS styling on API response displays

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # APOD-API-NASA
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/434fa586-d65c-42e1-bf98-f988a3f6c29a/deploy-status)](https://app.netlify.com/sites/apod-api-application/deploys)
-
 A web-based application to display a picture of the day provided by NASA's 'Picture of the Day' API. Or you can choose a date from the past. Works for both images and videos.
 
 **Technologies Used:**
 
--   Nasa APOD open API which can be found at https://api.nasa.gov/
--   HTML
--   CSS
--   Typescript
--   ReactJS
+- Nasa APOD open API which can be found at https://api.nasa.gov/
+- HTML
+- CSS
+- Typescript
+- ReactJS
 
 **Sample Photo**
 ![Screenshot (58)](https://user-images.githubusercontent.com/49052244/151297835-3ce41659-aa3f-4ec8-9c5b-2ee0137ed3fe.png)
--- src/components/ApiContentResponse.tsx
+++ src/components/ApiContentResponse.tsx
@@ -1,34 +1,76 @@
-import React from 'react'
-import axios, { AxiosRequestHeaders } from 'axios'
+import React, { useState } from 'react'
+import axios from 'axios'
 import dotenv from 'dotenv'
 import './ApiContentResponse.css'
 
 function ApiContentResponse() {
-  try {
-    const fetchData = async () => {
+  //prettier-ignore
+  const fetchData = async () => {
+    try {
       const apiKey = process.env.REACT_APP_NASA_API_KEY
-      const url: string = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
-      const apiResponse: AxiosRequestHeaders = await axios.get(url)
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+      const apiResponse = await axios.get(url)
 
-      // Display or do something w/ data
-      console.log(apiResponse)
+      if (apiResponse.status === 200) {
+        const apiData = apiResponse.data
+        console.log(apiData)
+
+        displayRequestedData(apiData)
+      } else throw new Error('Nasa seems to be having an issue with their server.')
+    } catch (err) {
+      console.log('Nasa seems to be having an issue with their server.')
     }
-    fetchData()
-  } catch (err: any) {
-    throw new Error(err)
   }
+
+  //prettier-ignore
+  const displayRequestedData = (data: any) => {
+    const dateEl = document.querySelector('#date') as HTMLParagraphElement
+    const titleEl = document.querySelector('#title') as HTMLHeadingElement
+    const copyrightEl = document.querySelector('#copyright') as HTMLElement
+    const photoExplanationEl = document.querySelector('#description') as HTMLParagraphElement
+    const todaysDate = new Date().toISOString().slice(0, 10) as string
+    const mediaSectionEl = document.querySelector('#media-section') as HTMLParagraphElement
+    const imageSectionEl = `<a id="img-new-tab" href="" target="_blank" rel="noopener">
+                              <div class="image-div"> 
+                                  <img id="image_of_the_day" src="" alt="image-by-nasa"> 
+                              </div>
+                            </a>` as any
+    const videoSectionEl = `<div class="video-div"> 
+                              <iframe id="videoLink" src="" frameborder="0"></iframe>
+                            </div>` as any
+
+    // Title, Date, Media Copyright, Photo Explanation
+    titleEl.innerHTML = data.title
+    dateEl.innerHTML = data.date
+    copyrightEl.innerHTML = data.copyright
+    photoExplanationEl.innerHTML = data.explanation
+
+    // Dynamically supply HTML element based on video or img API response
+    if (data.media_type === 'video') {
+      mediaSectionEl.innerHTML = videoSectionEl
+      videoSectionEl!.src = data.url
+    } else {
+      mediaSectionEl.innerHTML = imageSectionEl
+      document.querySelector<HTMLAnchorElement>('#img-new-tab')!.href =
+        data.hdurl
+      document.querySelector<HTMLImageElement>('#image_of_the_day')!.src =
+        data.url
+    }
+  }
+
+  fetchData()
 
   return (
     <>
-      <section>
-        <h2 id="title"></h2>
-        <p id="date"></p>
+      <section className="media-details-flex-wrapper">
+        <h2 id="title">Loading...</h2>
+        <p id="date">date</p>
         <p id="credits-wrapper">Media courtsey:</p>
-        <p id="copyright"></p>
+        <p id="copyright">NASA</p>
       </section>
 
-      <section id="media-section">
-        {/* <!-- Response from API goes here --> */}
+      <section className="media" id="media-section">
+        {/* <!-- Media Response from API goes here --> */}
       </section>
 
       <div className="description-div">
