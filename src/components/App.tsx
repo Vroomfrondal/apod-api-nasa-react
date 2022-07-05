f90ee8fd9084d3be980b351488d27bfa4a27b6cb
Vroomfrondal <lionscantcode@gmail.com>
2022-07-04 23:17:14 -0500

    Footer Component
    - Added Footer comp with socials links and message.
    
    - Added icon library
    
    - CSS styling on Footer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import ApiContentResponse from './ApiContentResponse'
+import Footer from './Footer'
 
 function App() {
   return (
     <>
       <Header />
       <ApiContentResponse />
+      <Footer />
     </>
   )
 }
