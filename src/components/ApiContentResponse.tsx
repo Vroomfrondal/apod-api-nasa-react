977d7403e69438f7c109efaf29b43847f502ffbb
Christopher DeLeon <lionscantcode@gmail.com>
2022-07-09 22:15:22 -0500

    Modal (#11) (#12)
    - Responsive Image Media-query fix for 1080p
    
    - Modal Inception & Toggle
    
    - Modal Hooks & CSS
    
    - state for openStatus of Modal
    - open, close, and content props
    - [CSS] wrapper to hide back content & Modal styling

--- src/components/ApiContentResponse.tsx
+++ src/components/ApiContentResponse.tsx
@@ -1,23 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import dotenv from 'dotenv'
 import { fetchData } from '../api/fetchAndDisplayNasaData'
+import { Modal } from '../components/Modal'
 import './ApiContentResponse.css'
 
 function ApiContentResponse() {
-  // Default API call onload
+  // Modal
+  const [openStatus, setOpenStatus] = useState(false)
+
   useEffect(() => {
-    const apiKey = process.env.REACT_APP_NASA_API_KEY!
-    const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`!
-    fetchData(baseUrl)
+    const fetchToday = async () => {
+      const apiKey = process.env.REACT_APP_NASA_API_KEY!
+      const baseUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`!
+      fetchData(baseUrl)
+    }
+    fetchToday().catch(console.error)
   }, [])
 
   return (
     <>
       <main className="body-container">
         <section className="media-details-flex-container">
-          <h2 className="media-title" id="media-title">
-            Loading...
-          </h2>
+          <h2 className="media-title" id="media-title"></h2>
           <p className="media-date" id="media-date"></p>
           <p className="credits-wrapper" id="credits-wrapper">
             Media courtsey:
@@ -26,13 +30,28 @@ function ApiContentResponse() {
         </section>
 
         <section className="horizontal-body-flex-container">
-          <div className="media-section-wrapper" id="media-section">
+          <div
+            className="media-section-wrapper"
+            id="media-section"
+            onClick={() => {
+              setOpenStatus(true)
+            }}
+          >
             {/*   <!-- Media Response from API dynamically sets here --> */}
           </div>
 
-          <div className="description-wrapper">
+          <Modal
+            open={openStatus}
+            onClose={() => {
+              setOpenStatus(false)
+            }}
+          >
+            Fancy Modal
+          </Modal>
+
+          {/* <div className="description-wrapper">
             <p className="description" id="media-description"></p>
-          </div>
+          </div> */}
         </section>
       </main>
     </>
